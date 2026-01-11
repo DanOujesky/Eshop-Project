57c6ec0b5f6e0f4f6f5fc1b19f2f30cafb3eaef6
DanOujesky <oujesky@spsejecna.cz>
2026-01-11 20:28:32 +0100

    fix set up errors

--- src/db/db.js
+++ src/db/db.js
@@ -2,3 +2,19 @@ import mysql from "mysql2/promise";
 import { config } from "../config/config.js";
 
 export const pool = mysql.createPool(config.db);
+
+export const testDbConnection = async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Připojení k databázi bylo úspěšné.");
+    connection.release();
+    return true;
+  } catch (error) {
+    console.error("Nepodařilo se připojit k databázi:");
+    console.error(
+      `Host: ${config.db.host}, User: ${config.db.user}, DB: ${config.db.name}`
+    );
+    console.error(`Důvod: ${error.message}`);
+    return false;
+  }
+};
--- src/server.js
+++ src/server.js
@@ -3,9 +3,18 @@ import express from "express";
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
+import { testDbConnection } from "./db/db.js";
 
-const PORT = process.env.PORT;
-const SERVER_URL = process.env.SERVER_URL;
+const PORT = process.env.PORT || 5001;
+const SERVER_URL = process.env.SERVER_URL || "http://localhost:";
+const requiredEnvs = ["DB_HOST", "DB_USER", "DB_NAME", "DB_PASS"];
+
+requiredEnvs.forEach((name) => {
+  if (!process.env[name]) {
+    console.error(`CHYBA: Chybí konfigurační proměnná ${name} v souboru .env!`);
+    process.exit(1);
+  }
+});
 
 const app = express();
 
@@ -16,6 +25,8 @@ app.use("/api/product", productRoutes);
 app.use("/api/order", orderRoutes);
 app.use("/api/admin", adminRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  const dbConnection = await testDbConnection();
+  if (!dbConnection) process.exit(1);
   console.log(`Server běží na ${SERVER_URL}:${PORT}`);
 });
