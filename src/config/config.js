319306efdd9b32a40ff1cd9d06a97d04cb7ff0ca
DanOujesky <oujesky@spsejecna.cz>
2026-01-11 20:58:35 +0100

    update config

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 export const config = {
   db: {
     host: process.env.DB_HOST,
+    port: process.env.PORT || 3036,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
